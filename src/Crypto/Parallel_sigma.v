4333101a5e07133c12d3337f37df1607c34123ad
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-01-15 17:59:39 +0000

    Parallel Composition

---
+++ src/Crypto/Parallel_sigma.v
@@ -0,0 +1,52 @@
+From Coq Require Import Setoid
+  setoid_ring.Field Lia Vector Utf8
+  Psatz Bool Pnat BinNatDef 
+  BinPos. 
+From Algebra Require Import 
+  Hierarchy Group Monoid
+  Field Integral_domain
+  Ring Vector_space.
+From Probability Require Import 
+  Prob Distr. 
+From Utility Require Import Util. 
+From ExtLib Require Import Monad. 
+From Crypto Require Import Sigma.
+  
+
+Import MonadNotation 
+  VectorNotations.
+
+#[local] Open Scope monad_scope.
+
+(* Discrete Logarithm Zero Knowlege Proof *) 
+Section DL. 
+  (* Underlying Field of Vector Space *)
+  Context 
+    {F : Type}
+    {zero one : F} 
+    {add mul sub div : F -> F -> F}
+    {opp inv : F -> F}
+    {Fdec: forall x y : F, {x = y} + {x <> y}}. 
+    (* decidable equality on Field *)
+
+  (* Vector Element *)
+  Context 
+    {G : Type} 
+    {gid : G} 
+    {ginv : G -> G}
+    {gop : G -> G -> G} 
+    {gpow : G -> F -> G}
+    {Gdec : forall x y : G, {x = y} + {x <> y}}. 
+    (* decidable equality on G *)
+    
+
+  #[local] Infix "^" := gpow.
+  #[local] Infix "*" := mul.
+  #[local] Infix "/" := div.
+  #[local] Infix "+" := add.
+  #[local] Infix "-" := sub.
+  
+  
+
+
+End DL.
