f014d46d10c8799565cb55cffc586b490af60118
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-07-07 14:25:53 +0100

    Approval Voting

---
+++ src/Examples/Approval.v
@@ -0,0 +1,47 @@
+From Stdlib Require Import Setoid
+  Lia Vector Utf8 Fin. 
+From Algebra Require Import
+  Hierarchy Group Monoid
+  Field Integral_domain
+  Ring Vector_space.
+From Utility Require Import 
+  Util.
+
+
+Section Approval.
+
+  (* Underlying Field of Vector Space *)
+  Context 
+    {F : Type}
+    {zero one : F} 
+    {add mul sub div : F -> F -> F}
+    {opp inv : F -> F}.
+
+  (* Vector Element *)
+  Context 
+    {G : Type} 
+    {gid : G} 
+    {ginv : G -> G}
+    {gop : G -> G -> G} 
+    {gpow : G -> F -> G}
+    {Hvec: @vector_space F (@eq F) zero one add mul sub 
+      div opp inv G (@eq G) gid ginv gop gpow}.
+
+  Local Infix "^" := gpow.
+  Local Infix "*" := mul.
+  Local Infix "+" := add.
+
+  Section Definitions.
+
+    (* Encryption Proof *)
+    
+
+  End Definitions.
+
+
+  Section Proofs.
+
+
+  End Proofs.
+
+End Approval.
