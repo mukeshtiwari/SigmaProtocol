e3d86b017eeeb7dec1584a3aa470baf2dcd128d3
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-08-05 14:12:40 +0100

    Finished Approval Voting

--- src/Frontend/Approval.v
+++ src/Frontend/Approval.v
@@ -7,7 +7,7 @@ From Algebra Require Import
 From Utility Require Import 
   Util.
 From Crypto Require Import 
-  Elgamal EncProof.
+  Elgamal EncProof Sigma.
 Import VectorNotations.
 Section Approval.
 
@@ -32,6 +32,7 @@ Section Approval.
   Local Infix "*" := mul.
   Local Infix "+" := add.
 
+  #[local] Notation "( a ; c ; r )" := (mk_sigma _ _ _ a c r).
 
   Section Definitions.
 
@@ -50,16 +51,61 @@ Section Approval.
       let ms := [g^zero; g^one] in 
       let sig_proof := 
         match Fdec m zero with 
-        | left _ => @generalised_construct_encryption_proof_elgamal_real F zero add mul sub 
-            opp G ginv gop gpow 0 Fin.F1 r uscs ms g h cp c (* real one goes to the first place Fin.F1 *)
-        | right _ =>  @generalised_construct_encryption_proof_elgamal_real F zero add mul sub 
-            opp G ginv gop gpow 0 (Fin.FS Fin.F1) r uscs ms g h cp c (* real one goes to the second place (Fin.FS Fin.F1) *)
+        | left _ => 
+          @generalised_construct_encryption_proof_elgamal_real F zero add mul sub 
+            opp G ginv gop gpow 0 Fin.F1 r uscs ms g h cp c (* real one goes to 
+            the first place Fin.F1 *)
+        | right _ =>  
+          @generalised_construct_encryption_proof_elgamal_real F zero add mul sub 
+            opp G ginv gop gpow 0 (Fin.FS Fin.F1) r uscs ms g h cp c (* real one 
+            goes to the second place (Fin.FS Fin.F1) *)
         end 
       in (cp, sig_proof).
 
 
 
+    (* verification of encryption proof *)
+    Definition verify_encryption_vote_proof (g h : G)
+       (cppf : G * G * @Sigma.sigma_proto F (G * G) 2 3 2) : bool :=
+       match cppf with 
+       | (cp, pf) => @generalised_accepting_encryption_proof_elgamal F zero add 
+        Fdec G ginv gop gpow Gdec _ [g^zero; g^one] g h cp pf
+      end.
+
+
+    (* encrypts the whole ballot. *)
+    Fixpoint encrypt_ballot_and_generate_enc_proof {n : nat}
+      (r : Vector.t F n) (g h : G) (m : Vector.t F n) 
+      (uscs : Vector.t (Vector.t F 3) n) (c : Vector.t F n) : 
+      Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n.
+    Proof.
+      destruct n as [| n].
+      +
+        exact [].
+      +
+        destruct (vector_inv_S r) as (rh & rt & _).
+        destruct (vector_inv_S m) as (mh & mt & _).
+        destruct (vector_inv_S uscs) as (uscsh & uscst & _).
+        destruct (vector_inv_S c) as (ch & ct & _).
+        exact ((encrypt_vote_and_generate_enc_proof rh g h mh uscsh ch) ::
+          (encrypt_ballot_and_generate_enc_proof n 
+          rt g h mt uscst ct)).
+    Defined.
+
+
     
+    Fixpoint verify_encryption_ballot_proof {n : nat} (g h : G)
+    (cppf : Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n) : bool.
+    Proof.
+      destruct n as [| n].
+      +
+        exact true.
+      +
+        destruct (vector_inv_S cppf) as (cppfh & cppft & _).
+        exact ((verify_encryption_vote_proof g h cppfh) && 
+          (verify_encryption_ballot_proof _ g h cppft))%bool.
+    Defined.
+
 
   End Definitions.
   Section Proofs.
@@ -89,29 +135,16 @@ Section Approval.
         reflexivity.  (* Directly reduces to (gʳ, g¹·hʳ) *)
     Qed.
 
-
-    Theorem decryption_correct : ∀ (x : F) (r : F) (g h : G) (m : F) 
-      (uscs : Vector.t F 3) (c : F), h = g^x ->
-      let (cp, _) := encrypt_vote_and_generate_enc_proof r g h m uscs c in 
-      g^m = @dec F G ginv gop gpow x cp.
-    Proof.
-      intros * ha.
-      unfold encrypt_vote_and_generate_enc_proof, enc, dec.
-      eapply dec_is_left_inv_of_enc with (x := x) (r := r).
-      exact ha.
-      unfold enc. reflexivity.
-    Qed.
-
    
-    Theorem proof_valid:
+    Theorem vote_proof_valid :
       ∀ (r : F) (g h : G) (m : F) (uscs : Vector.t F 3) (c : F),
         (m = zero ∨ m = one) →
-        let (cp, pf) := encrypt_vote_and_generate_enc_proof r g h m uscs c in
-        @generalised_accepting_encryption_proof_elgamal F zero add 
-        Fdec G ginv gop gpow Gdec _ [g^zero; g^one] g h cp pf = true.
+        verify_encryption_vote_proof g h 
+          (encrypt_vote_and_generate_enc_proof r g h m uscs c) = true.
     Proof.
       intros * ha.
-      unfold encrypt_vote_and_generate_enc_proof.
+      unfold encrypt_vote_and_generate_enc_proof, 
+      verify_encryption_vote_proof.
       destruct (Fdec m zero) as [fa | fa].
       +
         eapply generalised_construct_encryption_proof_elgamal_real_completeness.
@@ -148,6 +181,39 @@ Section Approval.
         rewrite Hwt. reflexivity. 
         typeclasses eauto.
     Qed.
+
+    (* ballot proof is valid *)
+    Theorem ballot_proof_valid :
+      ∀ (n : nat) (rs : Vector.t F n) (g h : G) 
+        (ms : Vector.t F n) (uscs : Vector.t (Vector.t F 3) n) (chs : Vector.t F n),
+        (∀ (i : Fin.t n), Vector.nth ms i = zero ∨ Vector.nth ms i = one) ->
+        verify_encryption_ballot_proof g h 
+          (encrypt_ballot_and_generate_enc_proof rs g h ms uscs chs) = true.
+    Proof.
+      induction n as [|n ihn].
+      +
+        intros * ha.
+        pose proof (vector_inv_0 rs) as hb.
+        pose proof (vector_inv_0 ms) as hc.
+        pose proof (vector_inv_0 uscs) as hd.
+        pose proof (vector_inv_0 chs) as he.
+        subst; cbn. reflexivity.
+      +
+        intros * ha.
+        destruct (vector_inv_S rs) as (rsh & rst & hb).
+        destruct (vector_inv_S ms) as (msh & mst & hc).
+        destruct (vector_inv_S uscs) as (uscsh & uscst & hd).
+        destruct (vector_inv_S chs) as (chsh & chst & he).
+        subst. simpl. eapply Bool.andb_true_iff.
+        split.
+        ++
+          eapply vote_proof_valid.
+          exact (ha Fin.F1).
+        ++
+          eapply ihn.
+          intro f.
+          exact (ha (Fin.FS f)).
+    Qed.  
 
   End Proofs.
 End Approval.
