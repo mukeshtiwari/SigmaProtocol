24c8bd149a3baee1ba340b1c488fece47b3a2ad8
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-08-06 19:22:10 +0100

    Added some code

--- src/Backend/Tally.v
+++ src/Backend/Tally.v
@@ -1,5 +1,5 @@
 From Stdlib Require Import Setoid
-  Lia Vector Utf8 Fin. 
+  Lia Vector Utf8 Fin Permutation.
 From Algebra Require Import
   Hierarchy Group Monoid
   Field Integral_domain
@@ -75,39 +75,114 @@ Section Tally.
     | winners : list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n) ->  
       list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n) -> 
       list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n) -> 
-      Vector.t bool n -> state.
+      Vector.t F n -> state.
 
-    Print verify_encryption_ballot_proof.
+    Print Permutation.
     Inductive count : state -> Type :=
-    | ax (ms : Vector.t (G * G) n) (pf : Vector.t (@sigma_proto F G 2 1 1) n) : 
-        Vector.fold_left  (fun acc '((c₁, c₂), pfv) => 
-          (acc && (@decryption_proof_accepting_conversations F 
-          G ginv gop gpow Gdec g h c₁ c₂ gid pfv))%bool) true 
+    (* 
+    - ax bootstraps the election 
+      ms is encryption of zero -- ms = (@encrypted_ballot F G gop gpow g h _
+        (repeat_ntimes n zero) rs)
+      pf is decryption proof that ms decryptions to g^zero.
+
+      Is it overkill? Can I not simply reveal the randomness rs used to 
+      produce ms? 
+    *)
+    | ax 
+      (ms : Vector.t (G * G) n) (pf : Vector.t (@sigma_proto F G 2 1 1) n) : 
+      Vector.fold_left  (fun acc '((c₁, c₂), pfv) => 
+        (acc && (@decryption_proof_accepting_conversations F 
+          G ginv gop gpow Gdec g h c₁ c₂ (g ^ zero) pfv))%bool) true 
         (@zip_with _ _ _ _ (fun cp pfcp => (cp, pfcp)) ms pf) = true ->
-        count (partial (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n))
-          (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) 
-          (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) ms) 
-          (* initial tally ms encryption of 0 *)
-    | cvalid (u : Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)
+      count (partial (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n))
+        (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) 
+        (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) ms)
+    (* 
+      u is a valid ballot with encryption proof of 0 or 1 
+      ms is the tally so far and nms is the new tally nms = ms + u 
+      us the ballot seen so far 
+      vbs is the valid ballots and included in the count 
+      inbs is the invalid ballots  
+      count (partial us vbs inbs ms) is the partial count seen so far
+    *)
+    | cvalid 
+      (u : Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)
       (ms nms : Vector.t (G * G) n)
       (us vbs inbs : list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) :
       count (partial us vbs inbs ms) -> 
+      Permutation us (vbs ++ inbs) -> 
       @verify_encryption_ballot_proof F zero one add Fdec G ginv gop gpow Gdec
         _ g h u = true (* u is valid vote *) ->
       nms = @mul_encrypted_ballots G gop _ ms (Vector.map fst u) ->
       count (partial (@List.cons _ u us) (@List.cons _ u vbs) inbs nms)
-    | cinvalid (u : Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)
+    (* 
+      u is invalid ballot 
+      ms is the tally so far 
+      us the ballot seen so far 
+      vbs is the valid ballots and included in the count 
+      inbs is the invalid ballots 
+      count (partial us vbs inbs ms) is the partial count seen so far
+    *)
+    | cinvalid 
+      (u : Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)
       (ms : Vector.t (G * G) n)
       (us vbs inbs : list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) :
       count (partial us vbs inbs ms) -> 
+      Permutation us (vbs ++ inbs) -> 
       @verify_encryption_ballot_proof F zero one add Fdec G ginv gop gpow Gdec
         _ g h u = false (* u is invalid vote *) ->
-      count (partial (@List.cons _ u us) vbs (@List.cons _ u inbs) ms).
+      count (partial (@List.cons _ u us) vbs (@List.cons _ u inbs) ms)
+    (*
+      us all the ballots in an election 
+      vbs is the valid ballots and included in the count 
+      inbs is the invalid ballots 
+      ms final tally 
+      ds is decryption of finally (it's group elements)
+      pt is discrete logarithm search over ds 
+    *)
+    | cfinish 
+      (us vbs inbs : list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) 
+      (ms : Vector.t (G * G) n) (ds : Vector.t G n) 
+      (pf : Vector.t (@sigma_proto F G 2 1 1) n) 
+      (pt : Vector.t F n) : 
+      count (partial us vbs inbs ms) -> 
+      Permutation us (vbs ++ inbs) -> 
+      (∀ (i : Fin.t n), g ^ (Vector.nth pt i) = Vector.nth ds i) -> 
+      Vector.fold_left  (fun acc '(p, ((c₁, c₂), pfv)) => 
+        (acc && (@decryption_proof_accepting_conversations F 
+          G ginv gop gpow Gdec g h c₁ c₂ p pfv))%bool) true
+        (@zip_with _ _ _ _ (fun pt cppfcp => (pt, cppfcp)) ds 
+        ((@zip_with _ _ _ _ (fun cp pfcp => (cp, pfcp)) ms pf))) = true ->
+      count (partial us vbs inbs ms) -> count (winners us vbs inbs pt).
 
 
 
-      
-      
+    Fixpoint compute_final_tally (rs : Vector.t F n) 
+      (us : list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)) : 
+      existsT (vbs inbs :  list (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n))
+        (ms : Vector.t (G * G) n), count (partial us vbs inbs ms).
+    Proof.
+      destruct us as [| u us'].
+      +
+        set (ms :=  (@encrypted_ballot F G gop gpow g h _ (repeat_ntimes n zero) rs)).
+        exists (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)),
+        (@List.nil (Vector.t (G * G * @Sigma.sigma_proto F (G * G) 2 3 2) n)), ms.
+        refine (ax ms _ _).
+        admit.
+      +
+        refine 
+          (match @verify_encryption_ballot_proof F zero one add Fdec G ginv gop gpow Gdec
+        _ g h u return _  
+        with 
+        | true => _
+        | false => _ 
+        end ). 
+        (* check if u is valid ballot or not? *)
+    Admitted.
+
+
+
+
 
 
   End Defs.
