92cdffe41032e2d798e00d7825af12e0d7efe33b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-08-12 11:19:39 +0100

    Parallel Sigma Extraction

---
+++ src/Extraction/ParaSigmalib/Extraction.v
@@ -0,0 +1,6 @@
+From Stdlib Require Import Extraction 
+ExtrOcamlBasic ExtrOcamlNativeString
+ExtrOcamlZBigInt.
+From Examples Require Import ParaSigmaIns.
+Set Extraction Output Directory ".". 
+Separate Extraction ParaSigmaIns.
