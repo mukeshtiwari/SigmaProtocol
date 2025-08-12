e5463d6a702432c96d5b8bedeac5b26f6b9b554c
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-08-12 11:06:45 +0100

    Code

--- src/Examples/SigmaIns.v
+++ src/Examples/SigmaIns.v
@@ -8,30 +8,38 @@ Section Ins.
   
   (* Prime q*)
   Definition q : Z := 11.
+
   (* Prime p *)
   Definition p : Z := 23.
+
   (* Prime Proof *)
   Theorem prime_q : Znumtheory.prime q : Prop : Type.
   Proof. compute. Admitted. 
+
   Theorem prime_p : Znumtheory.prime p : Prop : Type. 
   Proof. compute. Admitted.
-  (* safe prime *)
+  
+    (* safe prime *)
   Theorem safe_prime : p = (2 * q + 1)%Z.
   Proof. compute. exact eq_refl. Qed.
   (* secret key *)
+  
   Definition x : @Zp q :=
     {| Zpfield.v := 3; Zpfield.Hv := eq_refl : (3 mod q)%Z = 3%Z |}.
   (* group generator *)
+  
   Definition g : @Schnorr_group p q := 
     {| Schnorr.v := 2;
     Ha := conj eq_refl eq_refl : (0 < 2 < p)%Z;
     Hb := eq_refl : (2 ^ q mod p)%Z = 1%Z|}.
-  (* public key *)
+  
+    (* public key *)
   Definition h : @Schnorr_group p q := 
     Eval compute in @pow 2 23 11 safe_prime prime_p prime_q g x.
-  (* u is the randomness for commitment and c is the challenge. 
+  
+    (* u is the randomness for commitment and c is the challenge. 
   For the moment, it is random but I need to *)
-  Definition schnorr_protocol_construction (u c : @Zp q) : 
+  Definition schnorr_protocol_construction_ins (u c : @Zp q) : 
     @sigma_proto (@Zp q) (@Schnorr_group p q) 1 1 1.
   Proof.
     refine (@schnorr_protocol Zp zp_add zp_mul 
@@ -42,7 +50,7 @@ Section Ins.
     eapply prime_q. 
   Defined.
 
-  Definition schnorr_protocol_verification : 
+  Definition schnorr_protocol_verification_ins : 
     @sigma_proto (@Zp q) (@Schnorr_group p q) 1 1 1 -> bool.
   Proof. 
     intro transcript.
