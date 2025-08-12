492c97537f1363d41308ec887440ba99ba5a9bba
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-08-12 11:05:40 +0100

    Parallel Sigma Instace

---
+++ src/Examples/ParaSigmaIns.v
@@ -0,0 +1,76 @@
+
+From Stdlib Require Import Utf8 ZArith.
+From Crypto Require Import Sigma 
+  ParallelSigma.
+From Utility Require Import Zpstar.
+Import Vspace Schnorr Zpfield Zpgroup.
+
+Section Ins.
+
+  (* Prime q*)
+  Definition q : Z := 11.
+  
+  (* Prime p *)
+  Definition p : Z := 23.
+  
+  (* Prime Proof *)
+  Theorem prime_q : Znumtheory.prime q : Prop : Type.
+  Proof. compute. Admitted. 
+  
+  Theorem prime_p : Znumtheory.prime p : Prop : Type. 
+  Proof. compute. Admitted.
+  
+  (* safe prime *)
+  Theorem safe_prime : p = (2 * q + 1)%Z.
+  Proof. compute. exact eq_refl. Qed.
+  
+  (* secret key *)
+  Definition x : @Zp q :=
+    {| Zpfield.v := 3; Zpfield.Hv := eq_refl : (3 mod q)%Z = 3%Z |}.
+  
+  (* group generator *)
+  Definition g : @Schnorr_group p q := 
+    {| Schnorr.v := 2;
+    Ha := conj eq_refl eq_refl : (0 < 2 < p)%Z;
+    Hb := eq_refl : (2 ^ q mod p)%Z = 1%Z|}.
+  
+  (* public key *)
+  Definition h : @Schnorr_group p q := 
+    Eval compute in @pow 2 23 11 safe_prime prime_p prime_q g x.
+
+  (* us is the randomness for commitment and cs is the challenge. 
+  For the moment, it is random but I need to *)
+  Definition construct_parallel_conversations_schnorr_ins
+    (us cs : Vector.t (@Zp q) 2) : 
+    @sigma_proto (@Zp q) (@Schnorr_group p q) 2 2 2.
+  Proof.
+    refine 
+    (@construct_parallel_conversations_schnorr (@Zp q) 
+    zp_add zp_mul (@Schnorr_group p q) pow 2 x g us cs).
+    exact safe_prime.
+    exact prime_p.
+    exact prime_q.
+  Defined.
+
+  
+  Definition generalised_parallel_accepting_conversations_ins : 
+    @sigma_proto (@Zp q) (@Schnorr_group p q) 2 2 2 -> bool.
+  Proof.
+    intro transcript.
+    refine
+    (@generalised_parallel_accepting_conversations (@Zp q) (@Schnorr_group p q)  
+    mul_schnorr_group pow Schnorr.dec_zpstar _ g h transcript).
+    eapply prime_p.
+    eapply prime_q.
+    exact safe_prime.
+    eapply prime_p.
+    eapply prime_q.
+  Defined.
+
+End Ins.
+
+
+    
+
+
+
