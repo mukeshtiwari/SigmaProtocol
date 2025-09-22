7b5c867bd547b153d6d80673d0c344974b56ddfd
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-09-22 08:48:51 +0100

    And Sigma extraction code

---
+++ src/Extraction/AndSigmalib/Extraction.v
@@ -0,0 +1,6 @@
+From Stdlib Require Import Extraction 
+ExtrOcamlBasic ExtrOcamlNativeString
+ExtrOcamlZBigInt ExtrOcamlNatBigInt.
+From Examples Require Import AndSigmaIns.
+Set Extraction Output Directory ".". 
+Separate Extraction AndSigmaIns.
