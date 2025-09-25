4dd9710b30c9e1ccefcdb0930d38d324ebb4df71
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-09-25 10:14:31 +0100

    Encryption Proof

---
+++ src/Examples/EncProofIns.v
@@ -0,0 +1,152 @@
+From Stdlib Require Import Utf8 ZArith 
+Vector Fin.
+From Crypto Require Import Sigma EncProof.
+From Utility Require Import Zpstar.
+From Examples Require Import Prime.
+Import Vspace Schnorr Zpfield Zpgroup 
+VectorNotations.
+
+Section Ins.
+
+   (* Prime q*)
+  Definition q : Z := 2963.
+
+  (* Prime p *)
+  Definition p : Z := 2 * q + 1.
+  
+  (* Prime Proof *)
+  Theorem prime_q : Znumtheory.prime q : Prop : Type.
+  Proof. 
+    eapply is_prime_sqrt_correct.
+    compute; reflexivity.
+  Qed.
+
+  Theorem prime_p : Znumtheory.prime p : Prop : Type. 
+  Proof. 
+    eapply is_prime_sqrt_correct.
+    compute; reflexivity.
+  Qed.
+  
+  
+  (* safe prime *)
+  Theorem safe_prime : p = (2 * q + 1)%Z.
+  Proof. compute. exact eq_refl. Qed.
+
+  Definition g : @Schnorr_group p q.
+  Proof.
+    refine 
+    {| Schnorr.v := 12;
+    Ha := conj eq_refl eq_refl : (0 < 12 < p)%Z;
+    Hb := _ |}.
+    vm_cast_no_check (eq_refl (Zpow_facts.Zpow_mod 12 q p)).
+  Defined.
+
+   
+  Definition h : @Schnorr_group p q.
+  Proof.
+    refine 
+    {| Schnorr.v := 25;
+    Ha := conj eq_refl eq_refl : (0 < 25 < p)%Z;
+    Hb := _ |}.
+    vm_cast_no_check (eq_refl (Zpow_facts.Zpow_mod 25 q p)).
+  Defined.
+
+  Definition m₀ : @Schnorr_group p q.
+  Proof.
+    refine 
+    {| Schnorr.v := 36;
+    Ha := conj eq_refl eq_refl : (0 < 36 < p)%Z;
+    Hb := _ |}.
+    vm_cast_no_check (eq_refl (Zpow_facts.Zpow_mod 36 q p)).
+  Defined.
+
+  Definition m₁ : @Schnorr_group p q.
+  Proof.
+    refine 
+    {| Schnorr.v := 37;
+    Ha := conj eq_refl eq_refl : (0 < 37 < p)%Z;
+    Hb := _ |}.
+    vm_cast_no_check (eq_refl (Zpow_facts.Zpow_mod 36 q p)).
+  Defined.
+
+  Definition m₂ : @Schnorr_group p q.
+  Proof.
+    refine 
+    {| Schnorr.v := 38;
+    Ha := conj eq_refl eq_refl : (0 < 38 < p)%Z;
+    Hb := _ |}.
+    vm_cast_no_check (eq_refl (Zpow_facts.Zpow_mod 38 q p)).
+  Defined.
+
+  (* randomness used in encryption  *)
+  Definition r : @Zp q.
+  Proof.
+    refine {| Zpfield.v := 1876; Zpfield.Hv := _ |}.
+    vm_cast_no_check (eq_refl ((1876 mod q)%Z)).
+  Defined.
+
+  (* we encrypt m₂ using the randomness r. *)
+  Definition cp : (@Schnorr_group p q * @Schnorr_group p q).
+  Proof.
+    exact (@pow _ _ _ safe_prime prime_p prime_q g r, 
+     (@mul_schnorr_group p q prime_p prime_q m₂ 
+     (@pow _ _ _ safe_prime prime_p prime_q h r))).
+  Defined.
+
+  Definition construct_encryption_proof_elgamal_commitment_ins 
+    (us : Vector.t (@Zp q) 3) (cs : Vector.t (@Zp q) 2) : 
+    Vector.t (@Schnorr_group p q * @Schnorr_group p q) 3.
+  Proof.
+    refine(@construct_encryption_proof_elgamal_commitment
+    (@Zp q) zp_opp (@Schnorr_group p q) 
+    (@inv_schnorr_group 2 p q safe_prime prime_p prime_q)
+    (@mul_schnorr_group p q prime_p prime_q)
+    pow 1 1 (us ++ cs) [m₀; m₁; m₂] g h cp).
+    eapply prime_q.
+    exact safe_prime.
+    eapply prime_p.
+    eapply prime_q.
+  Defined.
+
+
+  Definition generalised_construct_encryption_proof_elgamal_real_ins 
+    (us : Vector.t (@Zp q) 3) (cs : Vector.t (@Zp q) 2) (c : (@Zp q)) :
+    @sigma_proto (@Zp q) (@Schnorr_group p q * @Schnorr_group p q) 3 4 3.
+  Proof.
+    refine(@generalised_construct_encryption_proof_elgamal_real (@Zp q)
+      Zpfield.zero zp_add zp_mul zp_sub zp_opp (@Schnorr_group p q)
+      (@inv_schnorr_group 2 p q safe_prime prime_p prime_q)
+      (@mul_schnorr_group p q prime_p prime_q)
+      pow 1 (Fin.FS (Fin.FS Fin.F1)) r (us ++ cs) 
+      [m₀; m₁; m₂] g h cp c).
+    eapply prime_q.
+    eapply prime_q.
+    exact safe_prime.
+    eapply prime_p.
+    eapply prime_q.
+  Defined.
+
+  Definition generalised_accepting_encryption_proof_elgamal_ins 
+    (proof : @sigma_proto (@Zp q) (@Schnorr_group p q * @Schnorr_group p q) 3 4 3) : bool.
+  Proof.
+    refine(@generalised_accepting_encryption_proof_elgamal 
+      (@Zp q) Zpfield.zero zp_add zp_dec 
+      (@Schnorr_group p q)
+      (@inv_schnorr_group 2 p q safe_prime prime_p prime_q)
+      (@mul_schnorr_group p q prime_p prime_q) pow 
+      Schnorr.dec_zpstar _  [m₀; m₁; m₂] g h cp proof).
+    eapply prime_q.
+    exact safe_prime.
+    eapply prime_p.
+    eapply prime_q.
+  Defined.
+
+End Ins.
+
+
+
+
+
+
+  
+
---
+++ src/Executable/EncProofcode/main.ml
@@ -0,0 +1,115 @@
+open EncProoflib.EncProofIns
+open Cryptokit
+open Hacl_star.Hacl.Keccak
+
+
+let rec vector_to_string (printer : 'a -> string) (v : 'a EncProoflib.VectorDef.t) : string =
+  match v with
+  | Coq_nil -> ""
+  | Coq_cons (h, _, r) ->
+      (printer h) ^ 
+      (if r = Coq_nil then "" else ", " ^ vector_to_string printer r)
+
+(* Specializations *)
+
+let vector_string =
+  vector_to_string Big_int_Z.string_of_big_int
+
+let vector_string_pair =
+  vector_to_string (fun (h1, h2) ->
+    "(" ^ Big_int_Z.string_of_big_int h1 ^ ", " ^ Big_int_Z.string_of_big_int h2 ^ ")")
+
+let proof_string (proof : (Z.t, Z.t * Z.t) EncProoflib.Sigma.sigma_proto) : string = 
+    match  proof with
+    | {announcement = a; challenge = c; 
+      response = r} -> "proof = {annoucement = "^ vector_string_pair a ^ " challenge = " ^ vector_string c ^ 
+      " response = " ^ vector_string r ^ "}" 
+
+
+let big_int_of_bytes_mod_q (s : bytes) (q : Z.t) : Z.t =
+  let n = ref Big_int_Z.zero_big_int in
+  Bytes.iter (fun c -> n := Big_int_Z.add_big_int
+    (Big_int_Z.shift_left_big_int !n 8)
+    (Big_int_Z.big_int_of_int (Char.code c))) s;
+  Big_int_Z.mod_big_int !n q 
+
+
+let rnd_list (q : Z.t) (n : int) : Z.t EncProoflib.VectorDef.t =
+  let rng = Random.device_rng "/dev/urandom" in
+  let buf = Bytes.create 4 in
+  let rec rnd_list_aux m =
+    match m with
+    | 0 -> EncProoflib.VectorDef.Coq_nil
+    | _ ->
+      let _ = rng#random_bytes buf 0 4 in
+      let v = big_int_of_bytes_mod_q buf q in
+      let vs = rnd_list_aux (m - 1) in
+      EncProoflib.VectorDef.Coq_cons (v, Big_int_Z.big_int_of_int 0, vs)
+  in
+  rnd_list_aux n
+
+
+let _ = 
+  let us = rnd_list EncProoflib.EncProofIns.q 3 in (* randomness for commitment *)
+  let cs = rnd_list EncProoflib.EncProofIns.q 2 in (* degree of freedom for cheating *)
+  let com =  vector_string_pair (EncProoflib.EncProofIns.construct_encryption_proof_elgamal_commitment_ins us cs) in 
+  let cha = "p = " ^ Big_int_Z.string_of_big_int EncProoflib.EncProofIns.p ^ ", q = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.q  ^ ", g = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.g ^ ", h  = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.h ^ ", m_UU2080_  = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.m_UU2080_ ^ ", m_UU2081_  = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.m_UU2081_ ^ ", m_UU2082_ = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.m_UU2082_ ^ ", fst cp = " ^ 
+    Big_int_Z.string_of_big_int (fst EncProoflib.EncProofIns.cp) ^ ", snd cp = " ^ 
+    Big_int_Z.string_of_big_int (snd EncProoflib.EncProofIns.cp) ^ ", com = " ^ com in 
+  let c = big_int_of_bytes_mod_q (shake256 ~msg:(String.to_bytes cha) ~size:4) EncProoflib.EncProofIns.q in
+  let proof = generalised_construct_encryption_proof_elgamal_real_ins us cs c in
+  let verify = 
+        match generalised_accepting_encryption_proof_elgamal_ins  proof with
+        | true -> "true"
+        | _ -> "false"
+  in
+  print_string ("p = " ^ Big_int_Z.string_of_big_int EncProoflib.EncProofIns.p ^ ", q = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.q  ^ ", g = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.g ^ ", h  = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.h ^ ", m_UU2080_  = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.m_UU2080_ ^ ", m_UU2081_  = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.m_UU2081_ ^ ", m_UU2082_ = " ^ 
+    Big_int_Z.string_of_big_int EncProoflib.EncProofIns.m_UU2082_ ^ ", cp = (" ^ 
+    Big_int_Z.string_of_big_int (fst EncProoflib.EncProofIns.cp) ^ ", " ^ 
+    Big_int_Z.string_of_big_int (snd EncProoflib.EncProofIns.cp) ^ "), com = " ^ com);
+  print_endline "";
+  print_string (proof_string proof);
+  print_endline "";
+  print_string verify;
+  
+(* 
+let _ = 
+    let us = rnd_list OrSigmalib.OrSigmaIns.q 3 in (* randomness for commitment *)
+    let cs = rnd_list OrSigmalib.OrSigmaIns.q 2 in (* degree of freedom for cheating *)
+    let com =  vector_string (OrSigmalib.OrSigmaIns.construct_or_conversations_schnorr_commitment_ins us cs) in 
+    let cha = "p = " ^ Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.p ^ ", q = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.q  ^ ", g = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.g ^ ", h_UU2081_  = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.h_UU2081_ ^ ", h_UU2082_  = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.h_UU2082_ ^ ", h_UU2083_  = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.h_UU2083_ ^ ", com = " ^ com in 
+    let c = big_int_of_bytes_mod_q (shake256 ~msg:(String.to_bytes cha) ~size:4) OrSigmalib.OrSigmaIns.q in
+    let proof = generalised_construct_or_conversations_schnorr_ins us cs c in 
+    let verify = 
+        match generalised_or_accepting_conversations_ins proof with
+        | true -> "true"
+        | _ -> "false"
+    in
+    print_string ("p = " ^ Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.p ^ ", q = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.q  ^ ", g = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.g ^ ", h_UU2081_  = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.h_UU2081_ ^ ", h_UU2082_  = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.h_UU2082_ ^ ", h_UU2083_  = " ^ 
+      Big_int_Z.string_of_big_int OrSigmalib.OrSigmaIns.h_UU2083_ ^ ", com = " ^ com);
+    print_endline "";
+    print_string (proof_string proof);
+    print_endline "";
+    print_string verify;
+
+*)
