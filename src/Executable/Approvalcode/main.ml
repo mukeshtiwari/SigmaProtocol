605c81051f199e686bea04d109d1b3176161ab33
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-10-09 16:27:23 +0100

    Printing function

--- src/Executable/Approvalcode/main.ml
+++ src/Executable/Approvalcode/main.ml
@@ -16,9 +16,28 @@ let rec vector_to_string (printer : 'a -> string) (sep : string) (v : 'a Approva
 let vector_string sep =
   vector_to_string Big_int_Z.string_of_big_int sep
 
-let vector_vector_string sep = 
-  vector_to_string (vector_to_string 
-    (fun (x, y) -> "(" ^ Big_int_Z.string_of_big_int x ^ ", " ^ Big_int_Z.string_of_big_int y ^ ")") sep) sep
+
+let vector_string_pair sep =
+  vector_to_string (fun (h1, h2) ->
+    "(" ^ Big_int_Z.string_of_big_int h1 ^ ", " ^ Big_int_Z.string_of_big_int h2 ^ ")") sep 
+
+
+let proof_string (proof : (Z.t, Z.t * Z.t) Approvallib.Sigma.sigma_proto) : string = 
+    match  proof with
+    | {announcement = a; challenge = c; 
+      response = r} -> "proof = {annoucement = "^ vector_string_pair "," a  ^ " challenge = " ^ vector_string  "," c ^ 
+      " response = " ^ vector_string  "," r ^ "}" 
+
+let cipher_string (cp : (Z.t * Z.t)) : string = 
+  match cp with 
+  |(cpa, cpb) -> "cipher text = (" ^ Big_int_Z.string_of_big_int cpa ^ ", " ^ Big_int_Z.string_of_big_int cpb ^ ")"
+
+let proof_and_enc_string (cppf : ((Z.t * Z.t) * (Z.t, Z.t * Z.t) sigma_proto)) : string = 
+  match cppf with
+  | (cp, pf) ->  cipher_string cp ^ " "^  proof_string pf 
+
+let vector_proof_and_enc_string (cppf : ((Z.t * Z.t) * (Z.t, Z.t * Z.t) sigma_proto) Approvallib.VectorDef.t) : string = 
+  vector_to_string proof_and_enc_string "; " cppf 
 
 
 let big_int_of_bytes_mod_q (s : bytes) (q : Z.t) : Z.t =
@@ -57,21 +76,21 @@ let generate_invalid_ballot (n : int) : Z.t Approvallib.VectorDef.t =
     rnd_list Approvallib.ApprovalIns.q n 
 
 let generate_valid_ballot_and_proof (n : int) : ((Z.t * Z.t) * (Z.t, Z.t * Z.t) sigma_proto) Approvallib.VectorDef.t = 
-  let ms = generate_valid_ballot 10 in 
-  let rs = rnd_list Approvallib.ApprovalIns.q 10 in 
-  let uscs = rnd_list_list Approvallib.ApprovalIns.q 3 10 in 
-  let cms = generate_ballot_commitment (Big_int_Z.big_int_of_int 10) rs ms uscs in
+  let ms = generate_valid_ballot n in 
+  let rs = rnd_list Approvallib.ApprovalIns.q n in 
+  let uscs = rnd_list_list Approvallib.ApprovalIns.q 3 n in 
+  let cms = generate_ballot_commitment (Big_int_Z.big_int_of_int n) rs ms uscs in
   let com = Approvallib.Vector.map (fun x -> 
       (vector_to_string (fun (u, v) -> "(" ^ Big_int_Z.string_of_big_int u ^ ", " ^ 
       Big_int_Z.string_of_big_int v ^ ")") "," x)) 
-      (Big_int_Z.big_int_of_int 10) cms in   
+      (Big_int_Z.big_int_of_int n) cms in   
   let cha = Approvallib.Vector.map (fun x -> 
     big_int_of_bytes_mod_q (String.to_bytes  ("p = " ^ Big_int_Z.string_of_big_int Approvallib.ApprovalIns.p ^ ", q = " ^ 
     Big_int_Z.string_of_big_int Approvallib.ApprovalIns.q  ^ ", g = " ^ 
     Big_int_Z.string_of_big_int Approvallib.ApprovalIns.g ^ ", h  = " ^ 
     Big_int_Z.string_of_big_int Approvallib.ApprovalIns.h ^ ", com = " ^ x)) Approvallib.ApprovalIns.q) 
-   (Big_int_Z.big_int_of_int 10) com in 
-  encrypt_ballot_and_generate_enc_proof_ins (Big_int_Z.big_int_of_int 10) rs ms uscs cha
+   (Big_int_Z.big_int_of_int n) com in 
+  encrypt_ballot_and_generate_enc_proof_ins (Big_int_Z.big_int_of_int n) rs ms uscs cha
 
 (* This would not pass the check *)
 let generate_invalid_ballot_and_proof (n : int) : ((Z.t * Z.t) * (Z.t, Z.t * Z.t) sigma_proto) Approvallib.VectorDef.t = 
@@ -97,7 +116,11 @@ let _ =
   let invalid_proof = generate_invalid_ballot_and_proof 10 in 
   let vf1 = verify_encryption_ballot_proof_ins  (Big_int_Z.big_int_of_int 10) valid_proof in 
   let vf2 = verify_encryption_ballot_proof_ins  (Big_int_Z.big_int_of_int 10) invalid_proof in 
-  print_string ("valid proof " ^ string_of_bool vf1);
+  print_string ("valid proof = " ^ vector_proof_and_enc_string valid_proof); 
   print_endline "";
-  print_string ("invalid proof " ^ string_of_bool vf2)
+  print_string ("invalid proof = " ^ vector_proof_and_enc_string invalid_proof); 
+  print_endline "";
+  print_string ("valid proof returns " ^ string_of_bool vf1);
+  print_endline "";
+  print_string ("invalid proof returns " ^ string_of_bool vf2)
 
