9b49669970ad6fe320679d31a7705e782a3d20cb
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-10-13 15:36:09 +0100

    removed unused function

--- src/Executable/Tallycode/parsing/ast.ml
+++ src/Executable/Tallycode/parsing/ast.ml
@@ -13,9 +13,3 @@ let vector_of_list (xs : 'a list) : 'a Tallylib.VectorDef.t =
       let tail_len = Big_int_Z.big_int_of_int (List.length lt) in
       Coq_cons (lh, tail_len, aux lt)
     in aux xs
-
-let vector_to_list (v : 'a  Tallylib.VectorDef.t) : 'a list =
-  let rec aux acc = function
-    | Coq_nil -> List.rev acc
-    | Coq_cons (x, _, tl) -> aux (x :: acc) tl
-in aux [] v
