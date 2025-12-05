6163e5696a0ecef363a5fd8d5277760b7d10b4ab
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-12-05 20:30:34 +0000

    IACR 2023 Verification code

--- src/Executable/HeliosVerifiercode/main.ml
+++ src/Executable/HeliosVerifiercode/main.ml
@@ -63,8 +63,8 @@ let rec print_count (bs : (Z.t, Z.t) HeliosTallylib.HeliosTally.count) : string 
 let _ = 
   let (bs, us, rs) = Parser.main Lexer.token (Lexing.from_channel stdin) in
   let result = HeliosTallylib.HeliosTallyIns.compute_final_count_ins 
-    (Big_int_Z.big_int_of_int 7) 
-    (Big_int_Z.big_int_of_int 2) bs us rs in 
+    (Big_int_Z.big_int_of_int 6) (Big_int_Z.big_int_of_int 2) 
+    (HeliosTallylib.HeliosTallyIns.h2023) bs us rs in 
   match result with
   | HeliosTallylib.Specif.Coq_existT (bfinal, 
     HeliosTallylib.Specif.Coq_existT (vbs, 
