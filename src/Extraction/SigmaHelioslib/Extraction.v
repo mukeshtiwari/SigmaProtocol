db4351062091de623462d15c73ab10cf371eb378
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2026-01-09 11:41:04 +0000

    Removed SigmaHelios because we already have Helios Verfier

--- src/Extraction/SigmaHelioslib/Extraction.v
+++
@@ -1,6 +0,0 @@
-From Stdlib Require Import Extraction 
-ExtrOcamlBasic ExtrOcamlNativeString
-ExtrOcamlZBigInt ExtrOcamlNatBigInt.
-From Examples Require Import SigmaInsHelios.
-Set Extraction Output Directory ".". 
-Separate Extraction SigmaInsHelios.
