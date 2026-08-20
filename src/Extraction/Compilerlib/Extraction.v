17a4f3fc072bfd89dd242ee602c211fdebbe5ff6
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2026-08-20 20:34:38 +0100

    Remove Compiler theory and dependents; extracted to standalone SigmaCompiler repo

--- src/Extraction/Compilerlib/Extraction.v
+++
@@ -1,7 +0,0 @@
-From Stdlib Require Import Extraction 
-ExtrOcamlBasic ExtrOcamlNativeString
-ExtrOcamlZBigInt ExtrOcamlNatBigInt.
-From Compiler Require Import CompilerIns.
-Extraction Blacklist String List Nat Ascii Byte Decimal.
-Set Extraction Output Directory ".". 
-Separate Extraction CompilerIns.
